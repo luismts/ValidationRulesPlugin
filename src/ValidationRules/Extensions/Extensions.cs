0d44a755e9d3c001a26cb5a7860cbeab8dead066
luismts <deivi_0326@hotmail.com>
2020-12-27 02:16:22 -0400

    Validator mapper added

--- src/ValidationRules/Extensions/Extensions.cs
+++ src/ValidationRules/Extensions/Extensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Reflection;
 
 namespace Plugin.ValidationRules.Extensions
 {
@@ -28,6 +27,47 @@ namespace Plugin.ValidationRules.Extensions
             }
 
             return result;
+        }
+
+        public static Model MapValidator<Model, Validator>(this Validator validator) where Model : new()
+        {
+            if (validator == null)
+                return default(Model);
+
+            Model newModel = new Model();
+
+            Type modelObjectType = newModel.GetType();
+            PropertyInfo[] modelPropList = modelObjectType.GetProperties();
+
+            Type validatorType = validator.GetType();
+            PropertyInfo[] validatorPropList = validatorType.GetProperties();
+
+            foreach (PropertyInfo validatorPropInfo in validatorPropList)
+            {
+                foreach (PropertyInfo modelPropInfo in modelPropList)
+                {
+                    if (modelPropInfo.Name == validatorPropInfo.Name)
+                    {
+                        try
+                        {
+                            PropertyInfo validatorProp = validatorPropInfo.PropertyType.GetProperty(nameof(Validatable<string>.Value));
+
+                            if (validatorProp == null)
+                                break;
+
+                            var validatorPropValue = validatorPropInfo.GetValue(validator); // Not working directly
+                            var propValue = validatorProp.GetValue(validatorPropValue, null);
+
+                            modelPropInfo.SetValue(newModel, propValue, null);
+                        }
+                        catch (Exception) { }
+
+                        break;
+                    }
+                }
+            }
+
+            return newModel;
         }
     }
 }
