604931fef836c411c355b5dd8792751342d197a7
luismts <deivi_0326@hotmail.com>
2020-12-27 02:15:12 -0400

    Validator interface removed

--- src/ValidationRules/Interfaces/IMapperValidator.cs
+++ src/ValidationRules/Interfaces/IMapperValidator.cs
@@ -2,10 +2,8 @@
 
 namespace Plugin.ValidationRules.Interfaces
 {
-    public interface IMapperValidator<in Validator, out Model> 
-        where Validator : class
-        where Model : class
+    public interface IMapperValidator<out Model> where Model : class
     {
-        Model Map(Validator input);
+        Model Map();
     }
 }
