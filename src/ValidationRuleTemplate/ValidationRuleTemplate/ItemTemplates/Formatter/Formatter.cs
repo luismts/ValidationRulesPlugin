84fa2c5881e303d74cdd7fa88b02e0c1b9e9de08
luismts <deivi_0326@hotmail.com>
2021-01-01 01:01:52 -0400

    Formatter template files created

---
+++ src/ValidationRuleTemplate/ValidationRuleTemplate/ItemTemplates/Formatter/Formatter.cs
@@ -0,0 +1,23 @@
+﻿using Plugin.ValidationRules.Interfaces;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(T value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            // Your implementation goes here
+
+            
+            return true;
+        }
+    }
+
+}
