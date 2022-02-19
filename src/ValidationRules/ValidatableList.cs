7abd92c74f15496200fb74363bd4ab3fc9f7de9e
luismts <deivi_0326@hotmail.com>
2022-02-19 15:42:24 -0400

    Add validatorList builder

--- src/ValidationRules/ValidatableList.cs
+++ src/ValidationRules/ValidatableList.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Input;
-using Plugin.ValidationRules.Extensions;
 using Plugin.ValidationRules.Interfaces;
 
 namespace Plugin.ValidationRules
